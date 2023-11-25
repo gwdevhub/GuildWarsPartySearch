21537cc531eda163ecb218c2e66d4df0a8d56eda
Alex Macocian <amacocian@yahoo.com>
2023-11-25 16:59:46 +0100

    Setup configurations

--- GuildWarsPartySearch/Launch/Program.cs
+++ GuildWarsPartySearch/Launch/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GuildWarsPartySearch.Server.Options;
-using GuildWarsPartySearch.Server.Tcp;
+using GuildWarsPartySearch.Server.ServerHandlers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using MTSC.ServerSide.Handlers;
@@ -25,6 +25,7 @@ public class Program
                     .WithHeartbeatEnabled(true)
                     .WithHeartbeatFrequency(server.ServiceManager.GetRequiredService<IOptions<ServerOptions>>().Value.HeartbeatFrequency ?? TimeSpan.FromSeconds(5)))
             .AddHandler(new ConnectionMonitorHandler())
+            .AddHandler(new StartupHandler())
             .AddServerUsageMonitor(new TickrateEnforcer() { TicksPerSecond = 60, Silent = true })
             .SetScheduler(new TaskAwaiterScheduler())
             .WithLoggingMessageContents(false);
---
+++ GuildWarsPartySearch/ServerHandlers/StartupHandler.cs
@@ -0,0 +1,33 @@
+﻿using GuildWarsPartySearch.Server.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MTSC;
+using MTSC.ServerSide;
+using MTSC.ServerSide.Handlers;
+
+namespace GuildWarsPartySearch.Server.ServerHandlers;
+
+public sealed class StartupHandler : IHandler
+{
+    private bool initialized;
+
+    public void ClientRemoved(MTSC.ServerSide.Server server, ClientData client) { }
+
+    public bool HandleClient(MTSC.ServerSide.Server server, ClientData client) => false;
+
+    public bool HandleReceivedMessage(MTSC.ServerSide.Server server, ClientData client, Message message) => false;
+
+    public bool HandleSendMessage(MTSC.ServerSide.Server server, ClientData client, ref Message message) => false;
+
+    public bool PreHandleReceivedMessage(MTSC.ServerSide.Server server, ClientData client, ref Message message) => false;
+
+    public void Tick(MTSC.ServerSide.Server server)
+    {
+        if (!this.initialized)
+        {
+            this.initialized = true;
+            var options = server.ServiceManager.GetRequiredService<IOptions<EnvironmentOptions>>();
+            server.Log($"Running environment {options.Value.Name}");
+        }
+    }
+}
