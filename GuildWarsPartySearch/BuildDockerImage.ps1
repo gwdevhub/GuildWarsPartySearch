4bd094b3defaca37e23569d963172ab18972c904
Macocian Alexandru Victor <amacocian@yahoo.com>
2023-11-25 17:24:36 +0100

    Fix release (#10)
    * Fix release
    
    * Try 2
    
    * Copy config to publish

--- GuildWarsPartySearch/BuildDockerImage.ps1
+++ GuildWarsPartySearch/BuildDockerImage.ps1
@@ -1,3 +1,4 @@
 dotnet publish -c Release
+Copy-Item -Path Config.Release.json -Destination ./bin/Release/net8.0/publish/Config.json
 docker build -t guildwarspartysearch.server .
 docker tag guildwarspartysearch.server guildwarspartysearch.azurecr.io/guildwarspartysearch.server
