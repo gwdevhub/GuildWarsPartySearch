391c9b5ca7e4bc66134c0e9a695eb32f60a9331e
Alex Macocian <amacocian@yahoo.com>
2023-11-25 20:38:38 +0100

    Improve content update

--- GuildWarsPartySearch/ServerHandlers/ContentManagementHandler.cs
+++ GuildWarsPartySearch/ServerHandlers/ContentManagementHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using GuildWarsPartySearch.Server.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -65,27 +66,50 @@ public sealed class ContentManagementHandler : IHandler
 
     private async Task UpdateContent(StorageAccountOptions options, ScopedLogger<ContentManagementHandler> scopedLogger)
     {
-        scopedLogger.LogInformation("Retrieving content");
+        scopedLogger.LogInformation("Checking content to retrieve");
         var serviceBlobClient = new BlobServiceClient(options.ConnectionString);
         var blobContainerClient = serviceBlobClient.GetBlobContainerClient(options.ContainerName);
         var blobs = blobContainerClient.GetBlobsAsync();
 
-        //TODO: Implement a better update. Currently we simply delete everything and redownload all the content
-        scopedLogger.LogInformation($"[{this.stagingFolder}] Deleting staging folder if it exists");
-        if (Directory.Exists(this.stagingFolder))
+        if (!Directory.Exists(this.stagingFolder))
         {
-            Directory.Delete(this.stagingFolder, true);
+            Directory.CreateDirectory(this.stagingFolder);
         }
 
         scopedLogger.LogInformation($"Retrieving blobs");
+        var blobList = new List<BlobItem>();
         await foreach(var blob in blobs)
+        {
+            blobList.Add(blob);
+        }
+
+        var stagingFolderFullPath = Path.GetFullPath(this.stagingFolder);
+        var stagedFiles = Directory.GetFiles(this.stagingFolder, "*", SearchOption.AllDirectories);
+        var filesToDelete = stagedFiles
+            .Select(f => Path.GetFullPath(f).Replace(stagingFolderFullPath, "").Replace('\\', '/').Trim('/'))
+            .Where(f => blobList.None(b => b.Name == f));
+        foreach (var file in filesToDelete)
+        {
+            scopedLogger.LogInformation($"[{file}] File not in blob. Deleting");
+            File.Delete($"{stagingFolderFullPath}\\{file}");
+        }
+
+        foreach(var blob in blobList)
         {
             var finalPath = Path.Combine(this.stagingFolder, blob.Name);
             var fileInfo = new FileInfo(finalPath);
             fileInfo.Directory!.Create();
+            if (fileInfo.Exists &&
+                fileInfo.CreationTimeUtc == blob.Properties.LastModified?.UtcDateTime &&
+                fileInfo.Length == blob.Properties.ContentLength)
+            {
+                scopedLogger.LogInformation($"[{blob.Name}] File unchanged. Skipping");
+                continue;
+            }
+
             var blobClient = blobContainerClient.GetBlobClient(blob.Name);
             using var fileStream = new FileStream(finalPath, FileMode.Create);
-            using var blobStream = await blobClient.OpenReadAsync(new Azure.Storage.Blobs.Models.BlobOpenReadOptions(false)
+            using var blobStream = await blobClient.OpenReadAsync(new BlobOpenReadOptions(false)
             {
                 BufferSize = 1024,
             }, CancellationToken.None);
@@ -93,6 +117,9 @@ public sealed class ContentManagementHandler : IHandler
             scopedLogger.LogInformation($"[{blob.Name}] Downloading blob");
             await blobStream.CopyToAsync(fileStream);
             scopedLogger.LogInformation($"[{blob.Name}] Downloaded blob");
+
+            fileInfo = new FileInfo(finalPath);
+            fileInfo.CreationTimeUtc = blob.Properties.LastModified?.UtcDateTime ?? DateTime.UtcNow;
         }
     }
 }
