9a71af7adce9af0d919060a068ae0a6d2583aeb0
Alex Macocian <amacocian@yahoo.com>
2023-11-26 22:30:15 +0100

    Optimize content storage

--- GuildWarsPartySearch/Services/Content/ContentRetrievalService.cs
+++ GuildWarsPartySearch/Services/Content/ContentRetrievalService.cs
@@ -5,10 +5,12 @@ using System.Extensions;
 using Microsoft.Extensions.Options;
 using System.Core.Extensions;
 
-namespace GuildWarsPartySearch.Server.BackgroundServices;
+namespace GuildWarsPartySearch.Server.Services.Content;
 
 public sealed class ContentRetrievalService : BackgroundService
 {
+    private readonly Dictionary<string, DateTime> fileMetadatas = [];
+
     private readonly ContentOptions contentOptions;
     private readonly StorageAccountOptions storageAccountOptions;
     private readonly ILogger<ContentRetrievalService> logger;
@@ -25,63 +27,66 @@ public sealed class ContentRetrievalService : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var scopedLogger = this.logger.CreateScopedLogger(nameof(this.ExecuteAsync), string.Empty);
-        while(!stoppingToken.IsCancellationRequested)
+        var scopedLogger = logger.CreateScopedLogger(nameof(this.ExecuteAsync), string.Empty);
+        while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-                await this.UpdateContent(stoppingToken);
+                await UpdateContent(stoppingToken);
             }
             catch (Exception ex)
             {
                 scopedLogger.LogError(ex, "Encountered exception while retrieving content");
             }
 
-            await Task.Delay(this.contentOptions.UpdateFrequency, stoppingToken);
+            await Task.Delay(contentOptions.UpdateFrequency, stoppingToken);
         }
     }
 
     private async Task UpdateContent(CancellationToken cancellationToken)
     {
-        var scopedLogger = this.logger.CreateScopedLogger(nameof(this.UpdateContent), string.Empty);
-        scopedLogger.LogInformation("Checking content to retrieve");
-        var serviceBlobClient = new BlobServiceClient(this.storageAccountOptions.ConnectionString);
-        var blobContainerClient = serviceBlobClient.GetBlobContainerClient(this.storageAccountOptions.ContainerName);
+        var scopedLogger = logger.CreateScopedLogger(nameof(this.UpdateContent), string.Empty);
+        scopedLogger.LogDebug("Checking content to retrieve");
+        var serviceBlobClient = new BlobServiceClient(storageAccountOptions.ConnectionString);
+        var blobContainerClient = serviceBlobClient.GetBlobContainerClient(storageAccountOptions.ContainerName);
         var blobs = blobContainerClient.GetBlobsAsync(cancellationToken: cancellationToken);
 
-        if (!Directory.Exists(this.contentOptions.StagingFolder))
+        if (!Directory.Exists(contentOptions.StagingFolder))
         {
-            Directory.CreateDirectory(this.contentOptions.StagingFolder);
+            Directory.CreateDirectory(contentOptions.StagingFolder);
         }
 
-        scopedLogger.LogInformation($"Retrieving blobs");
+        scopedLogger.LogDebug($"Retrieving blobs");
         var blobList = new List<BlobItem>();
         await foreach (var blob in blobs)
         {
             blobList.Add(blob);
         }
 
-        var stagingFolderFullPath = Path.GetFullPath(this.contentOptions.StagingFolder);
-        var stagedFiles = Directory.GetFiles(this.contentOptions.StagingFolder, "*", SearchOption.AllDirectories);
+        var stagingFolderFullPath = Path.GetFullPath(contentOptions.StagingFolder);
+        var stagedFiles = Directory.GetFiles(contentOptions.StagingFolder, "*", SearchOption.AllDirectories);
         var filesToDelete = stagedFiles
             .Select(f => Path.GetFullPath(f).Replace(stagingFolderFullPath, "").Replace('\\', '/').Trim('/'))
             .Where(f => blobList.None(b => b.Name == f));
         foreach (var file in filesToDelete)
         {
-            scopedLogger.LogInformation($"[{file}] File not in blob. Deleting");
+            scopedLogger.LogDebug($"[{file}] File not in blob. Deleting");
             File.Delete($"{stagingFolderFullPath}\\{file}");
+            fileMetadatas.Remove(file);
+
         }
 
         foreach (var blob in blobList)
         {
-            var finalPath = Path.Combine(this.contentOptions.StagingFolder, blob.Name);
+            var finalPath = Path.Combine(contentOptions.StagingFolder, blob.Name);
             var fileInfo = new FileInfo(finalPath);
             fileInfo.Directory!.Create();
             if (fileInfo.Exists &&
-                fileInfo.CreationTimeUtc == blob.Properties.LastModified?.UtcDateTime &&
-                fileInfo.Length == blob.Properties.ContentLength)
+                fileInfo.Length == blob.Properties.ContentLength &&
+                fileMetadatas.TryGetValue(blob.Name, out var lastChangeDate) &&
+                lastChangeDate == blob.Properties.LastModified?.UtcDateTime)
             {
-                scopedLogger.LogInformation($"[{blob.Name}] File unchanged. Skipping");
+                scopedLogger.LogDebug($"[{blob.Name}] File unchanged. Skipping");
                 continue;
             }
 
@@ -92,12 +97,12 @@ public sealed class ContentRetrievalService : BackgroundService
                 BufferSize = 1024,
             }, cancellationToken);
 
-            scopedLogger.LogInformation($"[{blob.Name}] Downloading blob");
+            scopedLogger.LogDebug($"[{blob.Name}] Downloading blob");
             await blobStream.CopyToAsync(fileStream, cancellationToken);
             scopedLogger.LogInformation($"[{blob.Name}] Downloaded blob");
 
             fileInfo = new FileInfo(finalPath);
-            fileInfo.CreationTimeUtc = blob.Properties.LastModified?.UtcDateTime ?? DateTime.UtcNow;
+            fileMetadatas[blob.Name] = blob.Properties.LastModified?.UtcDateTime ?? DateTime.UtcNow;
         }
     }
 }
