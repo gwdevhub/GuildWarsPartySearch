4782832d0bec68eb727328e6645ab369172a9bcb
Alex Macocian <amacocian@yahoo.com>
2023-11-26 01:30:39 +0100

    Setup http and https servers

--- GuildWarsPartySearch/Launch/Program.cs
+++ GuildWarsPartySearch/Launch/Program.cs
@@ -3,6 +3,7 @@ using GuildWarsPartySearch.Server.HttpModules;
 using GuildWarsPartySearch.Server.Options;
 using GuildWarsPartySearch.Server.ServerHandlers;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MTSC.ServerSide.Handlers;
 using MTSC.ServerSide.Schedulers;
@@ -16,7 +17,7 @@ public class Program
 
     private static async Task Main()
     {
-        var server = new MTSC.ServerSide.Server(8080);
+        var server = new MTSC.ServerSide.Server(443);
         server.ServiceCollection.SetupServices();
         server.ServiceManager.SetupServiceManager();
         server
@@ -34,6 +35,22 @@ public class Program
             .SetScheduler(new TaskAwaiterScheduler())
             .WithLoggingMessageContents(false);
 
-        await server.RunAsync(CancellationTokenSource.Token);
+        var verificationServer = new MTSC.ServerSide.Server(80);
+        verificationServer.ServiceCollection.AddScoped(_ => server.ServiceManager.GetRequiredService<ILogger<MTSC.ServerSide.Server>>());
+        verificationServer.ServiceCollection.AddScoped(_ => server.ServiceManager.GetRequiredService<ILogger<ContentModule>>());
+        verificationServer.ServiceCollection.AddScoped(_ => server.ServiceManager.GetRequiredService<IOptions<ContentOptions>>());
+        verificationServer.AddHandler(new HttpHandler()
+            .AddHttpModule(new ContentModule()))
+            .AddServerUsageMonitor(new TickrateEnforcer { TicksPerSecond = 10, Silent = true })
+            .SetScheduler(new TaskAwaiterScheduler())
+            .WithLoggingMessageContents(false);
+
+        //Ugly hack to copy the setup from the https server onto the http server
+        var serverTask = server.RunAsync(CancellationTokenSource.Token);
+        var verificationServerTask = verificationServer.RunAsync(CancellationTokenSource.Token);
+
+        await Task.WhenAll(
+            serverTask,
+            verificationServerTask);
     }
 }
