5958f09cc92b5d4ab420bc283de59d1e1bc55f39
Alex Macocian <amacocian@yahoo.com>
2024-01-10 18:05:03 +0200

    Disable strict host checking

--- GuildWarsPartySearch/DeployToVM.ps1
+++ GuildWarsPartySearch/DeployToVM.ps1
@@ -10,7 +10,7 @@ if (!(Test-Path $KeyFile)){
 	exit -1
 }
 
-ssh -i $KeyFile "$User@$Address" "rm -rf deploy/; mkdir deploy"
-scp -r -i $KeyFile "$($PathToEnvFileFolder)/envfile" "$($User)@$($Address):deploy/"
-ssh -i $KeyFile "$User@$Address" 'sudo docker stop $(sudo docker ps -a -q)'
-ssh -i $KeyFile "$User@$Address" "sudo docker pull alex4991/guildwarspartysearch.server:latest; nohup sudo docker run --env-file deploy/envfile -p 80:80 -p 443:443 alex4991/guildwarspartysearch.server:latest > stdout.txt 2> stderr.txt &"
+ssh -o "StrictHostKeyChecking=no" -i $KeyFile "$User@$Address" "rm -rf deploy/; mkdir deploy"
+scp -o "StrictHostKeyChecking=no" -r -i $KeyFile "$($PathToEnvFileFolder)/envfile" "$($User)@$($Address):deploy/"
+ssh -o "StrictHostKeyChecking=no" -i $KeyFile "$User@$Address" 'sudo docker stop $(sudo docker ps -a -q)'
+ssh -o "StrictHostKeyChecking=no" -i $KeyFile "$User@$Address" "sudo docker pull alex4991/guildwarspartysearch.server:latest; nohup sudo docker run --env-file deploy/envfile -p 80:80 -p 443:443 alex4991/guildwarspartysearch.server:latest > stdout.txt 2> stderr.txt &"
