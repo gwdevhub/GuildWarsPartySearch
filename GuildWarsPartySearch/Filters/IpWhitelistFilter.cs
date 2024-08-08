b8ab6189f1b1ddbf87296999bc484862896924fc
Alex Macocian <amacocian@yahoo.com>
2024-08-08 20:44:46 +0200

    Whitelist X-Forwarded-For

--- GuildWarsPartySearch/Filters/IpWhitelistFilter.cs
+++ GuildWarsPartySearch/Filters/IpWhitelistFilter.cs
@@ -2,22 +2,26 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Extensions;
 
-namespace GuildWarsPartySearch.Server.Filters
-{
-    public class IpWhitelistFilter : IAsyncActionFilter
-    {
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-        {
-            var db = context.HttpContext.RequestServices.GetRequiredService<IIpWhitelistDatabase>();
-            var ipAddresses = await db.GetWhitelistedAddresses(context.HttpContext.RequestAborted);
-            var address = context.HttpContext.Connection.RemoteIpAddress?.ToString();
-            if (ipAddresses.None(whiteListed => whiteListed == address))
-            {
-                context.Result = new ForbiddenResponseActionResult("Forbidden");
-                return;
-            }
+namespace GuildWarsPartySearch.Server.Filters;
 
-            await next();
+public class IpWhitelistFilter : IAsyncActionFilter
+{
+    private const string XForwardedForHeaderKey = "X-Forwarded-For";
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var db = context.HttpContext.RequestServices.GetRequiredService<IIpWhitelistDatabase>();
+        var ipAddresses = await db.GetWhitelistedAddresses(context.HttpContext.RequestAborted);
+        var address = context.HttpContext.Connection.RemoteIpAddress?.ToString();
+        context.HttpContext.Request.Headers.TryGetValue(XForwardedForHeaderKey, out var values);
+        if (ipAddresses.None(whiteListed => 
+            whiteListed == address ||
+            values.Contains(whiteListed)))
+        {
+            context.Result = new ForbiddenResponseActionResult("Forbidden");
+            return;
         }
+
+        await next();
     }
 }
