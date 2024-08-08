4872ae970c88b50f54b02c3ef4040c44b0e324a1
Alex Macocian <amacocian@yahoo.com>
2024-08-08 20:21:33 +0200

    Copy content to docker Fix tests

---
+++ GuildWarsPartySearch.Tests/Infra/TestHostLifetime.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace GuildWarsPartySearch.Tests.Infra;
+
+internal class TestHostLifetime : IHostApplicationLifetime
+{
+    public CancellationToken ApplicationStarted { get; }
+    public CancellationToken ApplicationStopped { get; }
+    public CancellationToken ApplicationStopping { get; }
+
+    public void StopApplication()
+    {
+        throw new NotImplementedException();
+    }
+}
--- GuildWarsPartySearch.Tests/Services/BotStatus/BotStatusServiceTests.cs
+++ GuildWarsPartySearch.Tests/Services/BotStatus/BotStatusServiceTests.cs
@@ -12,13 +12,13 @@ public sealed class BotStatusServiceTests
 
     public BotStatusServiceTests()
     {
-        this.botStatusService = new BotStatusService(new TestLoggerWrapper<BotStatusService>());
+        this.botStatusService = new BotStatusService(new TestHostLifetime(), new TestLoggerWrapper<BotStatusService>());
     }
 
     [TestMethod]
     public async Task AddBot_UniqueId_Succeeds()
     {
-        var result = await this.botStatusService.AddBot("uniqueId", new ClientWebSocket());
+        var result = await this.botStatusService.AddBot("uniqueId-148-3", new ClientWebSocket());
 
         result.Should().BeTrue();
     }
@@ -26,8 +26,8 @@ public sealed class BotStatusServiceTests
     [TestMethod]
     public async Task AddBot_DuplicateId_Fails()
     {
-        await this.botStatusService.AddBot("nonUniqueId", new ClientWebSocket());
-        var result = await this.botStatusService.AddBot("nonUniqueId", new ClientWebSocket());
+        await this.botStatusService.AddBot("nonUniqueId-148-3", new ClientWebSocket());
+        var result = await this.botStatusService.AddBot("nonUniqueId-148-3", new ClientWebSocket());
 
         result.Should().BeFalse();
     }
@@ -35,8 +35,8 @@ public sealed class BotStatusServiceTests
     [TestMethod]
     public async Task AddBot_MultipleUniqueIds_Succeed()
     {
-        await this.botStatusService.AddBot("uniqueId1", new ClientWebSocket());
-        var result = await this.botStatusService.AddBot("uniqueId2", new ClientWebSocket());
+        await this.botStatusService.AddBot("uniqueId1-148-3", new ClientWebSocket());
+        var result = await this.botStatusService.AddBot("uniqueId2-148-3", new ClientWebSocket());
 
         result.Should().BeTrue();
     }
@@ -55,8 +55,8 @@ public sealed class BotStatusServiceTests
     [TestMethod]
     public async Task RemoveBot_ExistingId_Succeeds()
     {
-        await this.botStatusService.AddBot("uniqueId", new ClientWebSocket());
-        var result = await this.botStatusService.RemoveBot("uniqueId");
+        await this.botStatusService.AddBot("uniqueId-148-3", new ClientWebSocket());
+        var result = await this.botStatusService.RemoveBot("uniqueId-148-3");
 
         result.Should().BeTrue();
     }
@@ -64,7 +64,7 @@ public sealed class BotStatusServiceTests
     [TestMethod]
     public async Task RemoveBot_NonExistingId_Fails()
     {
-        var result = await this.botStatusService.RemoveBot("uniqueId");
+        var result = await this.botStatusService.RemoveBot("uniqueId-148-3");
 
         result.Should().BeFalse();
     }
@@ -72,10 +72,10 @@ public sealed class BotStatusServiceTests
     [TestMethod]
     public async Task RemoveBot_MultipleRemoves_Fails()
     {
-        await this.botStatusService.AddBot("uniqueId", new ClientWebSocket());
-        await this.botStatusService.RemoveBot("uniqueId");
+        await this.botStatusService.AddBot("uniqueId-148-3", new ClientWebSocket());
+        await this.botStatusService.RemoveBot("uniqueId-148-3");
         
-        var result = await this.botStatusService.RemoveBot("uniqueId");
+        var result = await this.botStatusService.RemoveBot("uniqueId-148-3");
 
         result.Should().BeFalse();
     }
@@ -91,12 +91,13 @@ public sealed class BotStatusServiceTests
     [TestMethod]
     public async Task GetBots_WithBots_ReturnsExpectedBots()
     {
-        await this.botStatusService.AddBot("uniqueId1", new ClientWebSocket());
-        await this.botStatusService.AddBot("uniqueId2", new ClientWebSocket());
+        await this.botStatusService.AddBot("uniqueId1-148-3", new ClientWebSocket());
+        await this.botStatusService.AddBot("uniqueId2-148-3", new ClientWebSocket());
 
         var result = await this.botStatusService.GetBots();
 
         result.Should().HaveCount(2);
-        result.Should().BeEquivalentTo(["uniqueId1", "uniqueId2"]);
+        result.First().Id.Should().Be("uniqueId1-148-3");
+        result.Skip(1).First().Id.Should().Be("uniqueId2-148-3");
     }
 }
