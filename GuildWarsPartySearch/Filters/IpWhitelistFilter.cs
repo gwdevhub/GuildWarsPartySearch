13491a90b37cf8791a7a3b96ff423d6577330c5b
Alex Macocian <amacocian@yahoo.com>
2024-08-08 20:58:47 +0200

    Log X-Forwarded-For header

--- GuildWarsPartySearch/Filters/IpWhitelistFilter.cs
+++ GuildWarsPartySearch/Filters/IpWhitelistFilter.cs
@@ -1,5 +1,7 @@
 ﻿using GuildWarsPartySearch.Server.Services.Database;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using System.Core.Extensions;
 using System.Extensions;
 
 namespace GuildWarsPartySearch.Server.Filters;
@@ -8,12 +10,29 @@ public class IpWhitelistFilter : IAsyncActionFilter
 {
     private const string XForwardedForHeaderKey = "X-Forwarded-For";
 
+    private readonly IIpWhitelistDatabase database;
+    private readonly ILogger<IpWhitelistFilter> logger;
+
+    public IpWhitelistFilter(
+        IIpWhitelistDatabase database,
+        ILogger<IpWhitelistFilter> logger)
+    {
+        this.database = database.ThrowIfNull();
+        this.logger = logger.ThrowIfNull();
+    }
+
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        var db = context.HttpContext.RequestServices.GetRequiredService<IIpWhitelistDatabase>();
-        var ipAddresses = await db.GetWhitelistedAddresses(context.HttpContext.RequestAborted);
         var address = context.HttpContext.Connection.RemoteIpAddress?.ToString();
-        context.HttpContext.Request.Headers.TryGetValue(XForwardedForHeaderKey, out var values);
+        var scopedLogger = this.logger.CreateScopedLogger(nameof(this.OnActionExecutionAsync), address ?? string.Empty);
+        var ipAddresses = await this.database.GetWhitelistedAddresses(context.HttpContext.RequestAborted);
+        
+        scopedLogger.LogDebug($"Received request");
+        if (context.HttpContext.Request.Headers.TryGetValue(XForwardedForHeaderKey, out var values))
+        {
+            scopedLogger.LogDebug($"X-Forwarded-For {string.Join(',', values.Select(s => s))}");
+        }
+
         if (ipAddresses.None(whiteListed => 
             whiteListed == address ||
             values.Contains(whiteListed)))
