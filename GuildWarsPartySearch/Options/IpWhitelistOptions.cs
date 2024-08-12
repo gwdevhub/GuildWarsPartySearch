c6427a0ae3e8864625b2876583add74a2748e4cf
Jon <>
2024-08-12 09:39:15 +0100

    Re-added default whitelist localhost

--- GuildWarsPartySearch/Options/IpWhitelistOptions.cs
+++ GuildWarsPartySearch/Options/IpWhitelistOptions.cs
@@ -5,5 +5,5 @@ namespace GuildWarsPartySearch.Server.Options;
 public class IpWhitelistOptions
 {
     [JsonPropertyName(nameof(Addresses))]
-    public List<string> Addresses { get; set; } = [];
+    public List<string> Addresses { get; set; } = ["127.0.0.1"];
 }
