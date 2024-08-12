8bc2286e2b75bffc72f07c1a27f2af63d5973884
DubbleClick <m@pyc.ac>
2024-08-12 13:04:50 +0200

    add api key to config
    * Update config.example.sh (add api key)
    
    * Update run.sh

--- GuildWarsPartySearch.Bot/config.example.sh
+++ GuildWarsPartySearch.Bot/config.example.sh
@@ -10,9 +10,11 @@ EMAIL=""
 PASSWORD=""
 # Character name to travel to listen for party searches
 CHARACTER=""
-# Map ID to travel to listen for party searches
-MAP_ID="148"
+# Map ID to travel to listen for party searches, default embark beach
+MAP_ID="857"
 # Composite district enum to use (e.g. 1 = AMERICAN) - check source code!
 DISTRICT="1"
+# API Key, necessary
+WEBSOCKET_API_KEY="development"
 # Endpoint to send party searches to
-WEBSOCKET_URL="ws://hostname/party-search/update"
+WEBSOCKET_URL="ws://localhost/party-search/update"
