86fa815954f6782184feabe91e71b1e7eb8d9b15
Jon <>
2024-08-14 09:03:27 +0100

    Added redeploy script

---
+++ GuildWarsPartySearch.Bot/redeploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Stops a container matching the name if this directory, redeploys latest code from repo, starts container again
+
+BOT_FOLDER=$PWD
+BOT_NAME=${PWD##*/}
+
+echo $BOT_NAME;
+
+docker stop $BOT_NAME;
+# Clear any existing tmp folder
+rm -R /tmp$BOT_FOLDER;
+
+# Clone into tmp folder
+mkdir -p /tmp$BOT_FOLDER;
+cd /tmp$BOT_FOLDER;
+git clone --recursive https://github.com/gwdevhub/GuildWarsPartySearch.git
+
+# Copy new bot code back into original folder
+cp -ura ./GuildWarsPartySearch/GuildWarsPartySearch.Bot/* $BOT_FOLDER;
+rm -R $BOT_FOLDER/linuxbuild;
+chmod -R 777 $BOT_FOLDER;
+
+# Clear any existing tmp folder
+rm -R /tmp$BOT_FOLDER;
+
+docker start $BOT_NAME;
